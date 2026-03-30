e94d9ed365a089b6f4bffc156fcb43a6fbd00635
Thái <thaiqp124@gmail.com>
2026-03-30 19:40:27 +0700

    feat: transform into a multi-owner structure

--- apps/parking/admin.py
+++ apps/parking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Vehicle, FeeRule, ParkingLog, VehicleFace
+from .models import Vehicle, FeeRule, ParkingLog, VehicleFace, ParkingLot, ParkingSlot, Booking
 from .admin_site import custom_admin_site
 
 class VehicleAdmin(admin.ModelAdmin):
@@ -9,7 +9,7 @@ class VehicleAdmin(admin.ModelAdmin):
     autocomplete_fields = ('user',)
 
 class FeeRuleAdmin(admin.ModelAdmin):
-    list_display  = ('fee_type', 'amount', 'active', 'effective_from', 'effective_to')
+    list_display  = ('fee_type', 'parking_lot', 'amount', 'active', 'effective_from', 'effective_to')
     search_fields = ('fee_type',)
     list_filter   = ('fee_type',)
     ordering      = ('-effective_from',)
@@ -24,7 +24,22 @@ class VehicleFaceAdmin(admin.ModelAdmin):
     list_display  = ('id', 'vehicle', 'owner_name', 'relationship', 'face_img', 'face_vector', 'is_default', 'parent')
     search_fields = ('id', 'owner_name')
 
+class ParkinglotAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'owner', 'name', 'address', 'latitude', 'longitude', 'total_slots', 'image', 'threshold_release')
+    search_fields = ('id', 'owner')
+
+class ParkingSlotAdmin(admin.ModelAdmin):
+    list_display  = ('id', 'parking_lot', 'slot_number', 'is_vip', 'is_occupied', 'is_look_up', 'is_force_released', 'raw_index', 'column_index', 'floor')
+    search_fields = ('id', 'parking_lot', 'slot_number')
+
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'vehicle', 'slot', 'start_time', 'end_time', 'deposit_amount', 'status')
+    search_fields = ('id', 'user', 'vehicle', 'slot')
+
 custom_admin_site.register(Vehicle, VehicleAdmin)
 custom_admin_site.register(FeeRule, FeeRuleAdmin)
 custom_admin_site.register(ParkingLog, ParkingLogAdmin)
 custom_admin_site.register(VehicleFace, VehicleFaceAdmin)
+custom_admin_site.register(ParkingSlot, ParkingSlotAdmin)
+custom_admin_site.register(ParkingLot, ParkinglotAdmin)
+custom_admin_site.register(Booking, BookingAdmin)
--- apps/parking/models.py
+++ apps/parking/models.py
@@ -4,15 +4,25 @@ from cloudinary.models import CloudinaryField
 from django.db import models
 from ..users.models import User
 
+
 class ParkingStatus(models.TextChoices):
     IN = "IN", "Đang gửi"
     OUT = "OUT", "Đã lấy xe"
+
 
 class FeeType(models.TextChoices):
     MOTORCYCLE = "MOTORCYCLE", "Xe máy"
     CAR = "CAR", "Ô tô"
     TRUCK = "TRUCK", "Xe tải"
     BUS = "BUS", "Xe bus"
+
+
+class BookingStatus(models.TextChoices):
+    PENDING = "PENDING", "Chờ thanh toán cọc",
+    CONFIRMED = "CONFIRMED", "Đã đặt chỗ thành công",
+    CHECKED_IN = "CHECKED_IN", "Đã vào bãi",
+    CANCELLED = "CANCELLED", "Khách hàng hủy",
+    EXPIRED = "EXPIRED", "Quán hạn không đến (mất cọc)"
 
 
 class BaseModel(models.Model):
@@ -25,8 +35,26 @@ class BaseModel(models.Model):
         ordering = ['-id']
 
 
+class ParkingLot(BaseModel):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="parking_slots")
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    total_slots = models.PositiveIntegerField()
+    image = CloudinaryField(null=True, blank=True)
+
+    # Ngưỡng hạ rào tự động
+    threshold_release = models.FloatField(default=0.8)
+
+
+    def __str__(self):
+        return self.name
+
+
 class FeeRule(BaseModel):
     fee_type = models.CharField(max_length=10, choices=FeeType.choices)
+    parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE, related_name="fee_rules")
     amount = models.PositiveIntegerField(help_text="Giá (VNĐ)")
     effective_from = models.DateField(default=timezone.localdate)
     effective_to = models.DateField(null=True, blank=True)
@@ -65,7 +93,39 @@ class VehicleFace(BaseModel):
         return f"{self.vehicle.name} - {self.owner_name}"
 
 
+class ParkingSlot(BaseModel):
+    parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE, related_name="slots")
+    slot_number = models.CharField(max_length=50, unique=True)
+    is_vip = models.BooleanField(default=False) #Ô vip cần book trước
+    is_occupied = models.BooleanField(default=False) # Cảm biến báo có xe hay không
+    is_look_up = models.BooleanField(default=False)  # Rào chắn đang đóng hay mở
+    is_force_released = models.BooleanField(default=False) # Đánh đấu ô vip bị ép hạ rào do bãi xe quá tải
+
+    raw_index = models.PositiveIntegerField(help_text="Hàng thứ mấy")
+    column_index = models.PositiveIntegerField(help_text="Cột thứ mấy")
+    floor = models.IntegerField(default=1)
+
+    def __str__(self):
+        return self.slot_number
+
+
+class Booking(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bookings")
+    vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name="bookings")
+    slot = models.ForeignKey(ParkingSlot, on_delete=models.CASCADE, related_name="bookings")
+
+    start_time = models.DateTimeField(help_text="Thời gian xe đến")
+    end_time = models.DateTimeField(null=True, blank=True, help_text="Thời gian dự kiến rời đi")
+
+    deposit_amount = models.PositiveIntegerField(help_text="Phí đặt chỗ")
+    status = models.CharField(max_length=10, choices=BookingStatus.choices, default=BookingStatus.PENDING)
+
+    def __str__(self):
+        return f"Booking {self.vehicle.license_plate} - {self.slot.slot_number} - {self.start_time} - {self.end_time}"
+
+
 class ParkingLog(BaseModel):
+    parking_lot = models.ForeignKey(ParkingLot, on_delete=models.PROTECT, related_name="parking_logs")
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="parking_logs")
     vehicle_face = models.ForeignKey(VehicleFace, on_delete=models.CASCADE, related_name="parking_logs")
     vehicle = models.ForeignKey(Vehicle, on_delete=models.CASCADE, related_name="parking_logs")
@@ -83,3 +143,17 @@ class ParkingLog(BaseModel):
 
     def __str__(self):
         return f"Log {self.id} - {self.vehicle.license_plate}"
+
+
+# class DynamicPriceRule(BaseModel):
+#     """Quy tắc tăng giảm giá theo điều kiện"""
+#     name = models.CharField(max_length=50)
+#     parking_lot = models.ForeignKey(ParkingLot, on_delete=models.CASCADE, related_name="dynamic_price_rules")
+#
+#     start_date = models.DateField()
+#     end_date = models.DateField()
+#
+#     # Hệ số nhận
+#     multiplier = models.FloatField(default=1)
+#     # Độ ưu tiên
+#     priority = models.IntegerField(default=0, help_text="Độ ưu tiên nếu có nhiều quy tắc trùng nhau")
